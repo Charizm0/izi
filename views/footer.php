73ddb7b4d9073ca3dfd6aaa9e932bd4060279318
Charizm0 <subhadeepsarkar1995@gmail.com>
2016-01-24 18:42:49 +0530

    added the view for the footer

---
+++ views/footer.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+	<title>footer</title>
+	<link rel="stylesheet" type="text/css" href="../../css/bootstrap.min.css">
+	<script type="text/javascript" src = "../../js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container"	
+	<div class="col-md-4">
+		<strong>Sitemap</strong>
+		<ul>
+			<li>Home</li>
+			<li>Blog</li>
+			<li>Gallery</li>
+			<li>Store</li>
+		</ul>
+	</div>
+	<div class="col-md-4">
+		<strong>Events</strong>
+		<ul>
+			<li>Pen Down</li>
+			<li>Through the Lens</li>
+			<li>Blog Competition</li>
+		</ul>
+	</div>
+	<div class="col-md-4">
+		<strong>Follow Us</strong>
+		<ul>
+			<li>Facebook</li>
+			<li>Twitter</li>
+			<li>Instagram</li>
+			<li>Pinterest</li>
+			<li>YouTube</li>
+		</ul>
+	</div>
+	<div class="col-md-12">
+		&copy izifiso.com 2014-2016. Izifiso is a registered trademark.
+	</div>
+</div>	
+
+</body>
+</html>
+
