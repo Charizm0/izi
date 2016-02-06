7f59f331e0cf7fa3a2f611505b9263c7bd4977ff
Charizm0 <subhadeepsarkar1995@gmail.com>
2016-02-07 01:11:39 +0530

    added homepage beta

--- views/homepage.php
+++ views/homepage.php
@@ -4,19 +4,169 @@
     <meta charset="utf-8">
     <title>Izifiso-Home</title>
     <link rel="stylesheet" href="../../css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="../css/homepage.css" media="screen" title="no title" charset="utf-8">
   </head>
   <body>
     <?php
     @include 'navbar.php';
      ?>
 
-    <div class="container-fluid">
+    <div class="container-fluid home-wrapper">
       <div class="container">
-        <div class="col-md-9">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <div class="col-md-9">
+          <div class="banner">
+            <div class="banner-container">
+              <img src="6.jpg" alt="" />
+              <div class="controller">
+                <p>
+                  Santa Claus is coming to town
+                </p>
+                <span>Sign Up bitches!</span>
+              </div>
+              <div id="left"><span class="glyphicon glyphicon-chevron-left"></span>
 
+              </div>
+              <div id="right"><span class="glyphicon glyphicon-chevron-right"></span>
+
+              </div>
+
+            </div>
+
+          </div>
+          <div class="events">
+            <h3>Events</h3>
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+              <p>
+                lorem ipsum sit amet
+              </p>
+
+            </div></a>
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+              <p>
+                lorem ipsum sit amet
+              </p>
+
+            </div></a>
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+              <p>
+                lorem ipsum sit amet
+              </p>
+
+            </div></a>
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+              <p>
+                lorem ipsum sit amet
+              </p>
+
+            </div></a>
+
+          </div>
+          <div class="photos">
+            <h3>Photos</h3>
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+              <p>
+                lorem ipsum sit amet
+              </p>
+
+            </div></a>
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+              <p>
+                lorem ipsum sit amet
+              </p>
+
+            </div></a>
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+              <p>
+                lorem ipsum sit amet
+              </p>
+
+            </div></a>
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+              <p>
+                lorem ipsum sit amet
+              </p>
+
+            </div></a>
+
+          </div>
+          <div class="blog">
+            <h3>Blogs</h3>
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+              <p>
+                lorem ipsum sit amet
+              </p>
+
+            </div></a>
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+              <p>
+                lorem ipsum sit amet
+              </p>
+
+            </div></a>
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+              <p>
+                lorem ipsum sit amet
+              </p>
+
+            </div></a>
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+              <p>
+                lorem ipsum sit amet
+              </p>
+
+            </div></a>
+
+          </div>
         </div>
-        <div class="col-md-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
+        <div class="col-md-3">
         </div>
 
       </div>
