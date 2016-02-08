ce3d3d990586239ac8245eec4775882b6d3b20cd
Charizm0 <subhadeepsarkar1995@gmail.com>
2016-02-08 21:46:35 +0530

    2/8/2016

---
+++ views/profile.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Subhadeep</title>
+    <link rel="stylesheet" href="../css/profile.css" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body>
+    <div class="container">
+      <div class="coverpic">
+        <img src="6.jpg" alt="" />
+
+      </div>
+      <div class="pagecontents">
+        <div class="profilenav">
+          <div class="profilenavlist">
+            <ul>
+              <li>Uploads</li>
+              <li>Likes</li>
+              <li>Wishlist</li>
+              <li>Messages</li>
+              <li>About</li>
+            </ul>
+
+          </div>
+
+
+        </div>
+
+      </div>
+      <div class="dp">
+        <img src="https://pbs.twimg.com/profile_images/652407659975548928/-xM8ZT3l_400x400.jpg" alt="" />
+
+      </div>
+      <div class="userinfo">
+        <span class="userfullname">Subhadeep</span><br>
+        <span class="username">@savy</span><br>
+        <span class="bio">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incm,i ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</span><br>
+        <span class="bloglink"><a href="http://www.facebook.com">facebook.com</a></span>
+        
+
+
+      </div>
+      <div class="maincontent">
+
+      </div>
+    </div>
+
+  </body>
+</html>
