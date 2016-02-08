9562083d5e64974044156494ce5bb6eb753422cb
Charizm0 <subhadeepsarkar1995@gmail.com>
2016-02-08 21:52:47 +0530

    minor update

--- views/homepage.php
+++ views/homepage.php
@@ -13,7 +13,7 @@
 
     <div class="container-fluid home-wrapper">
       <div class="container">
-        <div class="col-md-9">
+        <div class="col-md-12">
           <div class="banner">
             <div class="banner-container">
               <img src="6.jpg" alt="" />
@@ -35,6 +35,17 @@
           </div>
           <div class="events">
             <h3>Events</h3>
+
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+              <p>
+                lorem ipsum sit amet
+              </p>
+
+            </div></a>
             <a href=""><div class="item">
               <div class="item-image">
                 <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
@@ -79,6 +90,17 @@
           </div>
           <div class="photos">
             <h3>Photos</h3>
+
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+              <p>
+                lorem ipsum sit amet
+              </p>
+
+            </div></a>
             <a href=""><div class="item">
               <div class="item-image">
                 <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
@@ -163,16 +185,26 @@
               </p>
 
             </div></a>
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+              <p>
+                lorem ipsum sit amet
+              </p>
+
+            </div></a>
 
           </div>
-        </div>
+        </div><!--
         <div class="col-md-3">
           <div class="right-side">
             <img class="col-md-12" src="6.jpg" alt="" /> <img class="col-md-12" src="6.jpg" alt="" />
-        
+
 
           </div>
-        </div>
+        </div>-->
 
       </div>
 
