baa25877f3a7cfbe00ff5a679c2872556c490f74
Charizm0 <subhadeepsarkar1995@gmail.com>
2016-02-10 01:05:55 +0530

    added the profile view

--- views/profile.php
+++ views/profile.php
@@ -2,7 +2,8 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Subhadeep</title>
+    <title>Sunny Leone</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../css/profile.css" media="screen" title="no title" charset="utf-8">
   </head>
   <body>
@@ -11,39 +12,39 @@
         <img src="6.jpg" alt="" />
 
       </div>
-      <div class="pagecontents">
-        <div class="profilenav">
-          <div class="profilenavlist">
-            <ul>
-              <li>Uploads</li>
-              <li>Likes</li>
-              <li>Wishlist</li>
-              <li>Messages</li>
-              <li>About</li>
-            </ul>
-
+      <div class="block2">
+        <div class="profilemenu">
+          <div class="menu">
+          <ul>
+            <li><a href="#">option1</a></li>
+            <li><a href="#">option2</a></li>
+            <li><a href="#">option3</a></li>
+            <li><a href="#">option4</a></li>
+            <li><a href="#">option5</a></li>
+          </ul>
           </div>
 
+        </div>
+        <div class="info1">
+          <div class="dp">
+          <img src="https://pbs.twimg.com/profile_images/696378410281504769/UcysQcal.jpg" alt="" />
+          </div>
+          <div class="stats">
+            <h1>Sunny Leone</h1>
+            <h3>(babydoll)</h3>
+            <p>
+              nursery school teacher
+            </p>
+            <span class="message">Message Sunny Leone</span>
 
+          </div>
+        </div>
+        <div class="info2">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
 
       </div>
-      <div class="dp">
-        <img src="https://pbs.twimg.com/profile_images/652407659975548928/-xM8ZT3l_400x400.jpg" alt="" />
 
-      </div>
-      <div class="userinfo">
-        <span class="userfullname">Subhadeep</span><br>
-        <span class="username">@savy</span><br>
-        <span class="bio">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incm,i ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</span><br>
-        <span class="bloglink"><a href="http://www.facebook.com">facebook.com</a></span>
-        
-
-
-      </div>
-      <div class="maincontent">
-
-      </div>
     </div>
 
   </body>
