2f441b6eccfe501ca1f0083c945f22907268d99e
Charizm0 <subhadeepsarkar1995@gmail.com>
2016-02-11 16:22:13 +0530

    homepagev2 test

---
+++ views/homepagev2.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Izifiso-Home</title>
+    <link rel="stylesheet" href="../../css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="../css/homepagev2.css" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body>
+    <?php
+    @include 'navbar.php';
+     ?>
+
+    <div class="container-fluid home-wrapper">
+      <div class="container">
+        <div class="col-md-12">
+          <div class="banner">
+            <div class="banner-container">
+              <img src="6.jpg" alt="" />
+              <div class="controller">
+
+                  Santa Claus is coming to town
+
+                <span>Sign Up bitches!</span>
+              </div>
+              <div id="left"><span class="glyphicon glyphicon-chevron-left"></span>
+
+              </div>
+              <div id="right"><span class="glyphicon glyphicon-chevron-right"></span>
+
+              </div>
+
+            </div>
+
+          </div>
+          <div class="events">
+            <h3>Events</h3>
+
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+              <div class="item-info">
+                <p>
+                  Blah blah
+                </p>
+
+              </div>
+
+
+
+
+            </div></a>
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+
+
+
+
+            </div></a>
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+
+
+
+
+            </div></a>
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+
+
+
+
+            </div></a>
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+
+
+
+
+            </div></a>
+
+          </div>
+          <div class="photos">
+            <h3>Photos</h3>
+
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+
+
+
+
+            </div></a>
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+
+
+
+
+            </div></a>
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+
+
+
+
+            </div></a>
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+
+
+
+
+            </div></a>
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+
+
+
+
+            </div></a>
+
+          </div>
+          <div class="blog">
+            <h3>Blogs</h3>
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+
+
+
+
+            </div></a>
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+
+
+
+
+            </div></a>
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+
+
+
+
+            </div></a>
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+
+
+
+
+            </div></a>
+            <a href=""><div class="item">
+              <div class="item-image">
+                <img src="http://www.shortday.in/wp-content/uploads/2015/05/priyanka-chopra.jpg" alt="" />
+
+              </div>
+
+
+
+
+            </div></a>
+
+          </div>
+        </div><!--
+        <div class="col-md-3">
+          <div class="right-side">
+            <img class="col-md-12" src="6.jpg" alt="" /> <img class="col-md-12" src="6.jpg" alt="" />
+
+
+          </div>
+        </div>-->
+
+      </div>
+
+    </div>
+
+  </body>
+</html>
