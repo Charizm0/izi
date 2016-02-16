d418e4c64f471f5625c8be21cc9a39a42e875609
Charizm0 <subhadeepsarkar1995@gmail.com>
2016-02-16 09:12:45 +0530

    photoviewer added

--- views/imageviewer.php
+++ views/imageviewer.php
@@ -1,23 +1,60 @@
 <html>
-	<head>
-		<title>Dynamically entered title of the image</title>
-		<link rel="stylesheet" href="../css/imageviewer.css">
-	</head>
-	<body>
-		<div class="wrapper">
-			<div class="window">
-				<div class="image">
-					<img src="https://scontent.xx.fbcdn.net/hphotos-xat1/v/t1.0-9/12109131_914554275294123_3966505629208276186_n.jpg?oh=4f420362161c8d96aa1f3cb187eed491&oe=5764338D">
-					<div class="details"></div>
-				</div>				
-				<div class="comment-section">
-					<div class="image-info">
-						<h4>Oindrila</h4>
-					</div>
-					<div class="comments"></div>
-					<div class="comment"></div>
-				</div>
-			</div>
-		</div>
-	</body>
+  <head>
+    <link rel="stylesheet" href="../css/test.css">
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="row-sm-height">
+          <div class="col-height col-middle">
+             <div class="image-background">
+               <div class="image-container">
+                <img src="https://scontent.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12670153_963027400446810_9121383558166261016_n.jpg?oh=85021d54ac5358fc1226ad4fdc1f4dda&oe=576CE279" />
+               </div>
+            </div>
+          </div>
+          <div class="col-height2 col-middle2">
+             <div class="image-info-background">
+                <div class="cc">
+                <div class="comment-container">
+                <p><a href="">Oindrila</a></p>
+                <p><b>Stupid Title</b></p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga ea voluptatem, iste repellat adipisci tempora, delectus saepe cupiditate dignissimos iure impedit perferendis in, beatae voluptate sapiente, pariatur libero ab odit.</p>
+                <div class="essentials">
+                  <span class="like"><b>Like</b></span>
+                  <button class="fbshare"><b>facebook</b></button>
+                  <button class="gshare"><b>google+</b></button>
+                </div>                  
+                <div comment-box>
+                  <form>
+                  <textarea class="cb"></textarea>
+                  <button class="button">comment</button>
+                  </form>
+                </div>
+                <div class="comments">
+                  <div class="each-comment">
+                    <span class="commenter"><a href="">Subhadeep</a></span>
+                    <p class="comment">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat saepe sunt ipsam laboriosam temporibus nostrum ea optio, dolore soluta. Similique nemo nihil quas sit adipisci eum, voluptate odit totam eveniet!</p>
+                  </div>
+                  <div class="each-comment">
+                    <span class="commenter"><a href="">Subhadeep</a></span>
+                    <p class="comment">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat saepe sunt ipsam laboriosam temporibus nostrum ea optio, dolore soluta. Similique nemo nihil quas sit adipisci eum, voluptate odit totam eveniet!</p>
+                  </div>
+                  <div class="each-comment">
+                    <span class="commenter"><a href="">Subhadeep</a></span>
+                    <p class="comment">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat saepe sunt ipsam laboriosam temporibus nostrum ea optio, dolore soluta. Similique nemo nihil quas sit adipisci eum, voluptate odit totam eveniet!</p>
+                  </div>
+                  <div class="each-comment">
+                    <span class="commenter"><a href="">Subhadeep</a></span>
+                    <p class="comment">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat saepe sunt ipsam laboriosam temporibus nostrum ea optio, dolore soluta. Similique nemo nihil quas sit adipisci eum, voluptate odit totam eveniet!</p>
+                  </div>
+                </div>
+               </div>
+             </div>
+            </div>
+          </div>                  
+        </div>
+      </div>
+    </div>
+  </body>
 </html>
--- views/test.php
+++ views/test.php
@@ -1,6 +1,6 @@
 <html>
   <head>
-    <link rel="stylesheet" href="..css/test.css">
+    <link rel="stylesheet" href="../css/test.css">
   </head>
   <body>
     <div class="container">
@@ -9,17 +9,50 @@
           <div class="col-height col-middle">
              <div class="image-background">
                <div class="image-container">
-                <img src="http://precisepet.com/wp-content/uploads/2011/02/PR_small_breed_puppy.jpg" alt="" />
+                <img src="https://scontent.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12670153_963027400446810_9121383558166261016_n.jpg?oh=85021d54ac5358fc1226ad4fdc1f4dda&oe=576CE279" />
                </div>
             </div>
           </div>
-          <div class="col-height col-middle">
-            <div class="image-background">
-              <div class="image-container">
-                <img src="https://c1.staticflickr.com/3/2421/3800569689_16827ec936_b.jpg" alt="" />
-              </div>
+          <div class="col-height2 col-middle2">
+             <div class="image-info-background">
+                <div class="cc">
+                <div class="comment-container">
+                <p><a href="">Oindrila</a></p>
+                <p><b>Stupid Title</b></p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga ea voluptatem, iste repellat adipisci tempora, delectus saepe cupiditate dignissimos iure impedit perferendis in, beatae voluptate sapiente, pariatur libero ab odit.</p>
+                <div class="essentials">
+                  <span class="like"><b>Like</b></span>
+                  <button class="fbshare"><b>facebook</b></button>
+                  <button class="gshare"><b>google+</b></button>
+                </div>                  
+                <div comment-box>
+                  <form>
+                  <textarea class="cb"></textarea>
+                  <button class="button">comment</button>
+                  </form>
+                </div>
+                <div class="comments">
+                  <div class="each-comment">
+                    <span class="commenter"><a href="">Subhadeep</a></span>
+                    <p class="comment">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat saepe sunt ipsam laboriosam temporibus nostrum ea optio, dolore soluta. Similique nemo nihil quas sit adipisci eum, voluptate odit totam eveniet!</p>
+                  </div>
+                  <div class="each-comment">
+                    <span class="commenter"><a href="">Subhadeep</a></span>
+                    <p class="comment">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat saepe sunt ipsam laboriosam temporibus nostrum ea optio, dolore soluta. Similique nemo nihil quas sit adipisci eum, voluptate odit totam eveniet!</p>
+                  </div>
+                  <div class="each-comment">
+                    <span class="commenter"><a href="">Subhadeep</a></span>
+                    <p class="comment">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat saepe sunt ipsam laboriosam temporibus nostrum ea optio, dolore soluta. Similique nemo nihil quas sit adipisci eum, voluptate odit totam eveniet!</p>
+                  </div>
+                  <div class="each-comment">
+                    <span class="commenter"><a href="">Subhadeep</a></span>
+                    <p class="comment">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat saepe sunt ipsam laboriosam temporibus nostrum ea optio, dolore soluta. Similique nemo nihil quas sit adipisci eum, voluptate odit totam eveniet!</p>
+                  </div>
+                </div>
+               </div>
+             </div>
             </div>
-          </div>
+          </div>                  
         </div>
       </div>
     </div>
